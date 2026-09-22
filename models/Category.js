e9a2edcbce246267a2010267afc5089bb500692d
Myner Soft <mynersoft@gmail.com>
2026-09-22 17:54:07 +0600

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,25 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const CategorySchema = new mongoose.Schema(
-	{
-		name: {
-			type: String,
-			required: [true, 'Category name is required'],
-			unique: true,
-			trim: true,
-		},
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
 
-		subCategories: {
-			type: [String],
-			default: [],
-		},
-	},
-	{
-		timestamps: true,
-	}
+    subCategories: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const Category =
-	mongoose.models.Category || mongoose.model('Category', CategorySchema);
-
-export default Category;
+export default mongoose.models.Category ||
+  mongoose.model("Category", CategorySchema);
